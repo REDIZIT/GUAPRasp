08c4e22348f31d58a157a356cc6b510efab4c0b7
REDIZIT <38188793+REDIZIT@users.noreply.github.com>
2022-10-14 20:55:22 +0300

    Pages moved to folder

--- App1/App1/Pages/App.xaml.cs
+++ App1/App1/Pages/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App1.Pages;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
--- App1/App1/TimeTable.cs
+++ App1/App1/TimeTable.cs
@@ -1,6 +1,5 @@
 ﻿using App1.Extensions;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
