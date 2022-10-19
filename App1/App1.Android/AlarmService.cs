10e7a1d3113e4ac1bec4ac5210ca5f5cb6251672
REDIZIT <38188793+REDIZIT@users.noreply.github.com>
2022-10-19 14:43:20 +0300

    Service and simple alert

---
+++ App1/App1.Android/AlarmService.cs
@@ -0,0 +1,91 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using AndroidX.Core.App;
+using App1;
+using App1.Droid;
+using System;
+using Xamarin.Forms;
+
+namespace InApp
+{
+    [Service]
+    public class AlarmService : Service
+    {
+        public static AlarmService Instance { get; private set; }
+
+        private Notification notification;
+        private DateTime targetDate;
+
+        public override void OnCreate()
+        {
+            Instance = this;
+
+            string NOTIFICATION_CHANNEL_ID = "no.jore.hajk";
+            string channelName = "test app service";
+            NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationImportance.None)
+            {
+                LockscreenVisibility = NotificationVisibility.Public
+            };
+            NotificationManager manager = (NotificationManager)GetSystemService(Context.NotificationService);
+            manager.CreateNotificationChannel(chan);
+
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+            notification = notificationBuilder
+                    .SetSmallIcon(Resource.Drawable.material_cursor_drawable)
+                    .SetContentTitle("App1")
+                    .SetContentText("Text")
+                    .SetContentIntent(BuildIntentToShowMainActivity())
+                    .SetPriority(1)
+                    .SetOngoing(false)
+                    .SetCategory(Notification.CategoryAlarm)
+                    //.AddAction(new NotificationCompat.Action()
+                    .Build();
+
+            // Enlist this instance of the service as a foreground service
+            StartForeground(1, notification);
+
+
+            targetDate = DateTime.Now.AddSeconds(10);
+
+            Device.StartTimer(new System.TimeSpan(0, 0, 1), () =>
+            {
+                if (DateTime.Now >= targetDate)
+                {
+                    Log.ShowAlert("Alert by time");
+                    return false;
+                }
+                return true;
+            });
+        }
+        public override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            Instance = null;
+        }
+
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+
+        public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+        {
+            Log.ShowAlert("On service start");
+
+            return StartCommandResult.Sticky;
+        }
+        
+        PendingIntent BuildIntentToShowMainActivity()
+        {
+            var notificationIntent = new Intent(this, typeof(MainActivity));
+            //notificationIntent.SetAction(Constants.ACTION_MAIN_ACTIVITY);
+            //notificationIntent.SetFlags(ActivityFlags.SingleTop | ActivityFlags.ClearTask);
+            //notificationIntent.PutExtra(Constants.SERVICE_STARTED_KEY, true);
+
+            var pendingIntent = PendingIntent.GetActivity(this, 0, notificationIntent, PendingIntentFlags.UpdateCurrent | PendingIntentFlags.Immutable);
+            return pendingIntent;
+        }
+    }
+}
--- App1/App1.Android/MainActivity.cs
+++ App1/App1.Android/MainActivity.cs
@@ -1,28 +1,50 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
+using Android.Content;
 using Android.Content.PM;
-using Android.Runtime;
 using Android.OS;
+using Android.Runtime;
+using InApp;
+using Xamarin.Forms;
 
 namespace App1.Droid
 {
     [Activity(Label = "GUAPTime", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        public static MainActivity Instance { get; private set; }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            Instance = this;
+
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            StartAlarm();
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        protected override void OnDestroy()
+        {
+            base.OnDestroy();
+
+            Instance = null;
+        }
+
+        private void StartAlarm()
+        {
+            if (InApp.AlarmService.Instance == null)
+            {
+                var intent = new Intent(ApplicationContext, typeof(AlarmService));
+                StartForegroundService(intent);
+            }
         }
     }
 }
